52f2982ae118b62de19636043296356b0fe18f21
Mukund Lakshman <yaymukund@gmail.com>
2014-11-29 21:32:52 -0500

    Concatenate bundle!

--- Brocfile.js
+++ Brocfile.js
@@ -2,18 +2,35 @@
 var compileModules = require('broccoli-es6-module-transpiler'),
     pickFiles = require('broccoli-static-compiler'),
     mergeTrees = require('broccoli-merge-trees'),
-    AMDFormatter = require('es6-module-transpiler-amd-formatter');
+    AMDFormatter = require('es6-module-transpiler-amd-formatter'),
+    concat = require('broccoli-concat'),
+    bower, app, concatenatedApp, sourceMaps;
 
-var bower = pickFiles('bower_components', {
+bower = pickFiles('bower_components', {
   srcDir: '/',
   destDir: '/bower'
 });
 
-var app = mergeTrees([
+app = mergeTrees([
   bower,
   'lib'
 ]);
 
-module.exports = compileModules(app, {
+app = compileModules(app, {
   formatter: new AMDFormatter()
 });
+
+concatenatedApp = concat(app, {
+  inputFiles: ['**/*.js'],
+  outputFile: '/aws-multipart.js',
+  wrapInEval: true
+  // header: commit sha.
+});
+
+sourceMaps = pickFiles(app, {
+  srcDir: '/',
+  files: ['**/*.map'],
+  destDir: '/'
+});
+
+module.exports = mergeTrees([concatenatedApp, sourceMaps]);
